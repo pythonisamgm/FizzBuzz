63c58dda7f5d8d1d05d1d288517156ffcbe5d461
Marta Gonzalez Morales <pythonisamgm@gmail.com>
2024-06-18 14:37:26 +0200

    iteration 2: add all functionallities in one method

--- tdd/src/main/java/com/example/FizzBuzz.java
+++ tdd/src/main/java/com/example/FizzBuzz.java
@@ -13,27 +13,19 @@ public class FizzBuzz {
         return this.number;
     }
 
-    public String printFizz() {
+    public String fizzBuzzKata() {
         if (this.number % 3 == 0) {
             return "Fizz";
-        } else {
-            return Integer.toString(this.number);
-        }
-    }
-
-    public String printBuzz() {
-        if (this.number % 5 == 0) {
+        }else if (this.number % 5 == 0) {
             return "Buzz";
-        }else {
+        }else if (this.number % 5 == 0 && this.number % 3 == 0) {
+            return "FizzBuzz";
+        }
+        else {
             return Integer.toString(this.number);
         }
     }
 
-    public String printFizzBuzz() {
-        if (this.number % 5 == 0 && this.number % 3 == 0) {
-            return "FizzBuzz";
-        } else {
-            return Integer.toString(this.number);
-        }
-    }
+    
+
 }
